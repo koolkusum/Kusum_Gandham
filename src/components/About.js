0d94d17b6c8dd9a490081058be4d9b6d369352f6
Kusum Gandham <koolkusum10@gmail.com>
2023-04-19 22:31:02 -0400

    image edit

--- src/components/About.js
+++ src/components/About.js
@@ -21,7 +21,7 @@ export default function About() {
             <div className= "aboue-me">
             As an aspiring full-stack developer, I'm passionate about exploring various areas of computer science, including data management, game design, and artificial intelligence. 
             In pursuit of my goals, I've taken relevant courses in Data Structures, Systems Programming, Discrete Structures, and Data 101.
-            To gain hands-on experience and hone my skills, I've participated in six hackathons to date and plan to attend many more in the future.
+            To gain hands-on experience and hone my skills, I've participated in seven hackathons to date and plan to attend many more in the future.
             When I'm not coding, I enjoy indulging in hobbies such as crocheting/knitting, tending to my plant collection, rollerblading, watching anime, and lifting weights.
             </div>
           </p>
@@ -47,13 +47,13 @@ export default function About() {
           </div>
         </div>
         <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-  <div className="about-img-container">
+  {/* <div className="about-img-container"> */}
     <img
       className="about-img animated"
       alt="hero"
-      src="./Me.jpg"
+      src="./me33.jpg"
     />
-  </div>
+  {/* </div> */}
 </div>
       </div>
     </section>
