0b9542a0478ef1ab20f5f1103e248c0318f6786e
Kusum Gandham <koolkusum10@gmail.com>
2023-05-06 22:38:53 -0400

    pink highlights

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,16 +10,16 @@ export default function Navbar() {
           </a>
         </a>
         <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-700 flex flex-wrap items-center text-base justify-center ml-auto">
-          <a href="#projects" className="mr-5 hover:text-white">
+          <a href="#projects" className="mr-5 hover:text-pink-400">
             Projects
           </a>
-          <a href="#skills" className="mr-5 hover:text-white">
+          <a href="#skills" className="mr-5 hover:text-pink-400">
             Skills
           </a>
-          <a href="#timeline" className="mr-5 hover:text-white">
+          <a href="#timeline" className="mr-5 hover:text-pink-400">
             Hackathons
           </a>
-          <a href="../Resume (9).pdf" target="_blank" className="mr-5 hover:text-white">
+          <a href="../Resume (9).pdf" target="_blank" className="mr-5 hover:text-pink-400">
             Resume
           </a>
         </nav>
